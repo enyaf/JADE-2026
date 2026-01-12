dcceaeffbea23452d9174aa90352a62fb53a643e
Enya Fang <enyafang01@gmail.com>
2026-01-12 11:43:42 -0500

    layout update and mountains!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+import mountains from './assets/mountains.png'
 
 function App() {
   return (
-    <main className="main">
-      <h1 className="name">Jane Doe</h1>
-      <p className="subtitle">
-        Student • Developer • Writer
-      </p>
+    <div className = "page">
+      <main className="main">
+        <h1 className="name">Enya Fang</h1>
+        <p className="subtitle">Econ-Stats @ Columbia University</p>
     </main>
+
+    <img
+      src={mountains}
+      alt="Mountain range"
+      className="mountain-footer"
+      />
+    </div>
   )
 }
 
