674c101ad47870febadbce1c3286d69024ab5c59
Enya Fang <enyafang01@gmail.com>
2026-01-14 17:10:29 -0500

    shelf

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -10,14 +10,15 @@ function Bookshelf() {
         </Link>
       </div>
       <header className="header">
-        <h1 className="name">Enya's Bookshelf</h1>
+        <h1 className="name">My Bookshelf</h1>
         <p className="blurb">
-          What I’m reading, what I’ve read, and what I go back to.
+          Current, past, and favourites. 
         </p>
       </header>
 
       <section>
-        <h2>Currently Reading</h2>
+        <h2>Currently Reading
+        </h2>
         <h2>Previously Read</h2>
         <h2>Favourites</h2>
       </section>
